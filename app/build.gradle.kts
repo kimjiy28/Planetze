820ba1320b63359f94ad4007cca2f9611a167ac5
bryntam <112902843+bryntam@users.noreply.github.com>
2024-11-14 00:05:12 -0500

    added HabitRepository.java and changed firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore")
 }
