567e2cd55d2119c3ce31bd567999e3d8656c852f
wenkanhh <nguyenvanquynhanh2005@gmail.com>
2024-11-26 04:06:36 -0500

    changes to calculateTransportationCO2() and calculateFoodCO2()

--- app/src/main/java/com/example/planetze/MainActivity.java
+++ app/src/main/java/com/example/planetze/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     // RadioGroups for Transportation
     private RadioGroup question1Group, question2Group, question3Group, question4Group, question5Group, question6Group, question7Group;
-    // RadioGroups for Transportation
+    // RadioGroups for Food
     private RadioGroup  question8Group, question10Group;
     private LinearLayout question2Layout, question9Layout;
     private RadioGroup question9BeefGroup, question9PorkGroup, question9ChickenGroup, question9FishGroup;
@@ -77,8 +77,8 @@ public class MainActivity extends AppCompatActivity {
         question19Group = findViewById(R.id.question19Group);
         question20Group = findViewById(R.id.question20Group);
         question21Group = findViewById(R.id.question21Group);
-        // Question 9 layout and checkboxes
 
+        // Question 9 layout and checkboxes
         question2Layout = findViewById(R.id.question2Layout);
         question9Layout = findViewById(R.id.question9Layout);
         question9BeefGroup = findViewById(R.id.question9BeefGroup);
@@ -149,14 +149,248 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     private double calculateTransportationCO2() {
-        return 0;
+        double consumptionCO2 = 0;
+
+        int q1Id = question1Group.getCheckedRadioButtonId();
+        int q2Id = question2Group.getCheckedRadioButtonId();
+        int q3Id = question3Group.getCheckedRadioButtonId();
+        int q4Id = question4Group.getCheckedRadioButtonId();
+        int q5Id = question5Group.getCheckedRadioButtonId();
+        int q6Id = question6Group.getCheckedRadioButtonId();
+        int q7Id = question7Group.getCheckedRadioButtonId();
+
+        RadioButton q1Button = findViewById(q1Id);
+        RadioButton q2Button = findViewById(q2Id);
+        RadioButton q3Button = findViewById(q3Id);
+        RadioButton q4Button = findViewById(q4Id);
+        RadioButton q5Button = findViewById(q5Id);
+        RadioButton q6Button = findViewById(q6Id);
+        RadioButton q7Button = findViewById(q7Id);
+
+        if (q1Button != null) {
+            String ans1 =  q1Button.getText().toString();
+
+            if (ans1.equals("Yes")) {
+                double rate = 0;
+                double distance = 0;
+
+                String ans2 = q2Button.getText().toString();
+                if (ans2.equals("Gasoline")) {
+                    rate = 0.24;
+                } else if (ans2.equals("Diesel")) {
+                    rate = 0.27;
+                } else if (ans2.equals("Hybrid")) {
+                    rate = 0.16;
+                } else if (ans2.equals("Electric")) {
+                    rate = 0.05;
+                }
+
+                String ans3 = q3Button.getText().toString();
+                if (ans3.equals("Up to 5,000km")) {
+                    distance = 5000;
+                } else if (ans3.equals("5,000-10,000km")) {
+                    distance = 10000;
+                } else if (ans3.equals("10,000-15,000km")) {
+                    distance = 15000;
+                } else if (ans3.equals("15,000-20,000km")) {
+                    distance = 20000;
+                } else if (ans3.equals("20,000-25,000km")) {
+                    distance = 25000;
+                } else if (ans3.equals("More than 25,000km")) {
+                    distance = 35000;
+                }
+
+                consumptionCO2 += (rate * distance);
+            }
+        }
+
+        if (q4Button != null) {
+            String ans4 = q4Button.getText().toString();
+            String ans5 = q5Button.getText().toString();
+
+            if (ans4.equals("Never")) {
+                consumptionCO2 += 0;
+            }
+
+            if (ans4.equals("Occasionally (1-2 times/week)")) {
+                if (ans5.equals("Under 1 hour")) {
+                    consumptionCO2 += 246;
+                } else if (ans5.equals("1-3 hours")) {
+                    consumptionCO2 += 819;
+                } else if (ans5.equals("3-5 hours")) {
+                    consumptionCO2 += 1638;
+                } else if (ans5.equals("5-10 hours")) {
+                    consumptionCO2 += 3071;
+                } else if (ans5.equals("More than 10 hours")) {
+                    consumptionCO2 += 4095;
+                }
+            }
+
+            if (ans4.equals("Frequently (3-4 times/week)")) {
+                if (ans5.equals("Under 1 hour")) {
+                    consumptionCO2 += 573;
+                } else if (ans5.equals("1-3 hours")) {
+                    consumptionCO2 += 1911;
+                } else if (ans5.equals("3-5 hours")) {
+                    consumptionCO2 += 3822;
+                } else if (ans5.equals("5-10 hours")) {
+                    consumptionCO2 += 7166;
+                } else if (ans5.equals("More than 10 hours")) {
+                    consumptionCO2 += 9555;
+                }
+            }
+
+            if (ans4.equals("Always (5+ times/week)")) {
+                if (ans5.equals("Under 1 hour")) {
+                    consumptionCO2 += 573;
+                } else if (ans5.equals("1-3 hours")) {
+                    consumptionCO2 += 1911;
+                } else if (ans5.equals("3-5 hours")) {
+                    consumptionCO2 += 3822;
+                } else if (ans5.equals("5-10 hours")) {
+                    consumptionCO2 += 7166;
+                } else if (ans5.equals("More than 10 hours")) {
+                    consumptionCO2 += 9555;
+                }
+            }
+
+        }
+
+        if (q6Button != null) {
+            String ans6 = q6Button.getText().toString();
+
+            if (ans6.equals("None")) {
+                consumptionCO2 += 0;
+            } else if (ans6.equals("1-2 flights")) {
+                consumptionCO2 += 225;
+            } else if (ans6.equals("3-5 flights")) {
+                consumptionCO2 += 600;
+            } else if (ans6.equals("6-10 flights")) {
+                consumptionCO2 += 1200;
+            } else if (ans6.equals("More than 10 flights")) {
+                consumptionCO2 += 1800;
+            }
+        }
+
+        if (q7Button != null) {
+            String ans7 = q7Button.getText().toString();
+
+            if (ans7.equals("None")) {
+                consumptionCO2 += 0;
+            } else if (ans7.equals("1-2 flights")) {
+                consumptionCO2 += 825;
+            } else if (ans7.equals("3-5 flights")) {
+                consumptionCO2 += 2200;
+            } else if (ans7.equals("6-10 flights")) {
+                consumptionCO2 += 4400;
+            } else if (ans7.equals("More than 10 flights")) {
+                consumptionCO2 += 6600;
+            }
+        }
+
+        return consumptionCO2;
     }
 
     private double calculateFoodCO2() {
-        return 0;
+        double consumptionCO2 = 0;
+
+        int q8Id = question8Group.getCheckedRadioButtonId();
+        int q9PId = question9PorkGroup.getCheckedRadioButtonId();
+        int q9BId = question9BeefGroup.getCheckedRadioButtonId();
+        int q9CId = question9ChickenGroup.getCheckedRadioButtonId();
+        int q9FId = question9FishGroup.getCheckedRadioButtonId();
+        int q10Id = question10Group.getCheckedRadioButtonId();
+
+        RadioButton q8Button = findViewById(q8Id);
+        RadioButton q9PButton = findViewById(q9PId);
+        RadioButton q9BButton = findViewById(q9BId);
+        RadioButton q9CButton = findViewById(q9CId);
+        RadioButton q9FButton = findViewById(q9FId);
+        RadioButton q10Button = findViewById(q10Id);
+
+        if (q8Button != null) {
+            String ans8 = q8Button.getText().toString();
+            String ans9P = q9PButton.getText().toString();
+            String ans9B = q9BButton.getText().toString();
+            String ans9C = q9CButton.getText().toString();
+            String ans9F = q9FButton.getText().toString();
+
+            if (ans8.equals("Vegetarian")) {
+                consumptionCO2 += 1000;
+            } else if (ans8.equals("Vegan")) {
+                consumptionCO2 += 500;
+            } else if (ans8.equals("Pescatarian")) {
+                consumptionCO2 += 1500;
+            } else if (ans8.equals("Meat-based")) {
+                if (ans9B.equals("Daily")) {
+                    consumptionCO2 += 2500;
+                } else if (ans9B.equals("Frequently (3-5 times/week)")) {
+                    consumptionCO2 += 1900;
+                } else if (ans9B.equals("Occasionally (1-2 times/week)")) {
+                    consumptionCO2 += 1300;
+                } else consumptionCO2 += 0;;
+
+                if (ans9P.equals("Daily")) {
+                    consumptionCO2 += 1450;
+                } else if (ans9B.equals("Frequently (3-5 times/week)")) {
+                    consumptionCO2 += 860;
+                } else if (ans9B.equals("Occasionally (1-2 times/week)")) {
+                    consumptionCO2 += 450;
+                } else consumptionCO2 += 0;;
+
+                if (ans9C.equals("Daily")) {
+                    consumptionCO2 += 950;
+                } else if (ans9B.equals("Frequently (3-5 times/week)")) {
+                    consumptionCO2 += 600;
+                } else if (ans9B.equals("Occasionally (1-2 times/week)")) {
+                    consumptionCO2 += 200;
+                } else consumptionCO2 += 0;;
+
+                if (ans9F.equals("Daily")) {
+                    consumptionCO2 += 800;
+                } else if (ans9B.equals("Frequently (3-5 times/week)")) {
+                    consumptionCO2 += 500;
+                } else if (ans9B.equals("Occasionally (1-2 times/week)")) {
+                    consumptionCO2 += 150;
+                } else consumptionCO2 += 0;;
+            }
+        }
+
+        if (q10Button != null) {
+            String ans10 = q10Button.getText().toString();
+
+            if (ans10.equals("Never")) {
+                consumptionCO2 += 0;
+            } else if (ans10.equals("Rarely")) {
+                consumptionCO2 += 23.4;
+            } else if (ans10.equals("Occasionally")) {
+                consumptionCO2 += 70.2;
+            } else if (ans10.equals("Frequently")) {
+                consumptionCO2 += 140.4;
+            }
+        }
+
+        return consumptionCO2;
     }
 
     private double calculateHousingCO2() {
+        double consumptionCO2 = 0;
+
+        int q11Id = question11Group.getCheckedRadioButtonId();
+        int q12Id = question12Group.getCheckedRadioButtonId();
+        int q13Id = question13Group.getCheckedRadioButtonId();
+        int q14Id = question14Group.getCheckedRadioButtonId();
+        int q15Id = question15Group.getCheckedRadioButtonId();
+        int q16Id = question16Group.getCheckedRadioButtonId();
+        int q17Id = question17Group.getCheckedRadioButtonId();
+
+        RadioButton q11Button = findViewById(q11Id);
+        RadioButton q12Button = findViewById(q12Id);
+        RadioButton q13Button = findViewById(q13Id);
+        RadioButton q14Button = findViewById(q14Id);
+        RadioButton q15Button = findViewById(q15Id);
+        RadioButton q16Button = findViewById(q16Id);
+        RadioButton q17Button = findViewById(q17Id);
         return 0;
     }
 
