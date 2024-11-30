f039fa33a880cc1e96dd40622a399dd2186693d6
Jiyeon <04jemma@gmail.com>
2024-11-30 18:55:34 -0500

    fixed errors

--- app/src/main/java/com/example/planetze/PersonalVehicleFragment.java
+++ app/src/main/java/com/example/planetze/PersonalVehicleFragment.java
@@ -7,14 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class PersonalVehicleFragment extends Fragment {
 
-    private String distance, vehicle;
-    private TextView tvDistance, tvVehicle;
+    private String distance, type;
+    private TextView tvDistance, tvType;
+    private Spinner typeSpinner;
+    private ArrayAdapter<CharSequence> typeAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,37 @@ public class PersonalVehicleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_personal_vehicle, container, false);
+
+        typeSpinner = view.findViewById(R.id.spType);
+        typeAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.vehicle, R.layout.spinner);
+        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        typeSpinner.setAdapter(typeAdapter);
+        typeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                type = typeSpinner.getSelectedItem().toString();
+//                int parentId = parent.getId();
+//                if (parentId == R.id.spMealType) {
+//                    switch(category) {
+//                        case "Gasoline":
+//                            break;
+//                        case "Diesel":
+//                            break;
+//                        case "Hybrid":
+//                            break;
+//                        case "Electric":
+//                           break;
+//                        default:
+//                            break;
+//                    }
+//                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
         return view;
     }
 }
